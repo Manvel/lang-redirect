9644ae33ba6a3b64dddcf0dae57556a2f33a3857
Manvel Saroyan <saroyanm@gmail.com>
2018-11-17 15:34:35 +0400

    Added second test case

--- test.js
+++ test.js
@@ -1,5 +1,7 @@
 const {getLocaleToPageMap, getLocale} = require("./index");
 const assert = require("assert");
+const jsdom = require("jsdom");
+const {JSDOM} = jsdom;
 
 const locales = [
   {
@@ -22,13 +24,43 @@ const locales = [
     user: ["en-US", "ru", "ru-RU", "hy-AM", "hy", "en"],
     result: "ru_RU"
   }
-]
+];
 
-describe("Testing markdown-it-html-entities using markdown.render():", () => {
+const htmls = [
+{
+  html: `
+<html>
+<link rel="alternate" href="https://cmints.io/ru/" hreflang="ru">
+<link rel="alternate" href="https://cmints.io/de/" hreflang="de">
+<link rel="alternate" href="https://cmints.io/es/" hreflang="es">
+</html>
+  `,
+  result: {"ru": "https://cmints.io/ru/", "de": "https://cmints.io/de/", "es": "https://cmints.io/es/"}
+},
+{
+  html: `
+<html>
+<link rel="alternate" href="https://cmints.io/ru/">
+</html>
+  `,
+  result: {}
+}
+];
+
+describe("Testing getRedirect() function:", () => {
   locales.forEach(({page, user, result}) =>
   {
     it(`getRedirect(["${user.join('"')}"], ["${page.join('"')}"]): Should output: ${result}`, () => {
       assert.equal(getLocale(user, page), result);
+    });
+  });
+});
+
+describe("Testing getLocaleToPageMap() function:", () => {
+  htmls.forEach(({html, result}) =>
+  {
+    it(`${html} should return ${result}`, () => {
+      assert.deepStrictEqual(getLocaleToPageMap(new JSDOM(html).window.document), result);
     });
   });
 });
